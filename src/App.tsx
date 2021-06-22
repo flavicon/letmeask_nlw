66967743c4da7db61b48dc53cb59d126816d5f34
Flavio Silva <flavio.devjs@gmail.com>
2021-06-22 07:28:20 -0300

    Create page NewRoom, routes and signIn with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './styles/global.scss';
 
 function App() {
   return (
-	  <Home />
+	<BrowserRouter>
+		<Route path="/" exact component={Home} />
+		<Route path="/rooms/new" component={NewRoom} />
+	</BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,51 @@
+import { useHistory } from 'react-router-dom';
+
+import { firebase, auth } from '../services/firebase';
+import { Button } from '../components/Button';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home() {
+	const history = useHistory();
+
+	function handleCreateRoom() {
+		const provider = new firebase.auth.GoogleAuthProvider();
+
+		auth.signInWithPopup(provider).then(result => {
+			console.log(result);
+
+			history.push('/rooms/new')
+		})
+
+	}
+
 	return (
-		<div>
+		<div id="page-auth">
 			<aside>
 				<img src={illustrationImg} alt="ilustração question and ask" />
 				<strong>Crie salas de Q&amp;A ao-vivo</strong>
 				<p>Tire as dúvidas da sua audiência em tempo-real</p>
 			</aside>
 			<main>
-				<div>
+				<div className="main-content">
 					<img src={logoImg} alt="Letmeask" />
-					<button>
+					<button onClick={handleCreateRoom} className="create-room">
 						<img src={googleIconImg} alt="logo do google"/>
 						Crie sua sala com o google
 					</button>
-					<div>Ou entre em uma sala</div>
+					<div className="separator">Ou entre em uma sala</div>
 					<form>
 						<input
 							type="text"
 							placeholder="Digite o código da sala"
 						/>
-						<button type="submit">
+						<Button type="submit">
 							Entrar na sala
-						</button>
+						</Button>
 					</form>
 				</div>
 			</main>
